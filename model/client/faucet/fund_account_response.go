e386f47d7480527908a4e26c2b023a2ad2d2b185
PR <creature@localhost.localdomain>
2024-03-11 14:34:16 -0700

    fix faucet response

--- model/client/faucet/fund_account_response.go
+++ model/client/faucet/fund_account_response.go
@@ -3,9 +3,9 @@ package faucet
 import "github.com/CreatureDev/xrpl-go/model/transactions/types"
 
 type FundAccountResponse struct {
-	Account FaucetAccount           `json:"account"`
-	Amount  types.XRPCurrencyAmount `json:"amount"`
-	TxHash  types.Hash128           `json:"transactionHash"`
+	Account FaucetAccount `json:"account"`
+	Amount  uint64        `json:"amount"`
+	TxHash  types.Hash128 `json:"transactionHash"`
 }
 
 type FaucetAccount struct {
